65d4661dc3e2a2685a569463e62d3218f8f7a979
Dranelyh <kylianbdu13@gmail.com>
2025-02-13 16:01:03 +0100

    initialisation

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".tab-button");
+    const contents = document.querySelectorAll(".tab-content");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Supprime la classe active de tous les boutons et contenus
+            buttons.forEach(btn => btn.classList.remove("active"));
+            contents.forEach(content => content.classList.remove("active"));
+
+            // Active l'onglet cliqué
+            button.classList.add("active");
+            document.getElementById(`tab-${button.dataset.tab}`).classList.add("active");
+        });
+    });
+});
